5736235a2ba7a70dbc8ee50a2c92bc3eef8e0a8c
Mooseyyyy <hoekb24@gmail.com>
2023-03-02 13:02:04 -0700

    GenDatabase development

--- GenDatabase.cpp
+++ GenDatabase.cpp
@@ -3,10 +3,31 @@
 #include <vector>
 #include <string>
 
-void GenDatabase1K () {}
+void GenDatabase1K () {
+  std::string output;
+  //Create file
+  //Open File
+  for (int i = 0; i<1000; i++) {
+    int transaction = genRandomInt();
+    output = GenTransaction(transaction); 
+    //Write output to file
+  }
+  //Close File
+}
 void GenDatabase10K () {}
 void GenDatabase50K () {}
 void GenDatabase100K () {}
+
+int genRandomInt () {
+  //Random int generator for transaction amount
+  std::default_random_engine re;
+  re.seed(time(NULL)); 
+  std::uniform_int_distribution<int> uni(5, 15);
+
+  int randomNum = uni(re);
+  
+  return randomNum;
+}
 
 void populateVector(vector<std::string>& items) {
     for(int i = 0; i<100; i++) {
@@ -15,7 +36,15 @@ void populateVector(vector<std::string>& items) {
 }
 
 string GenTransaction(int num) {
+  std::string trans;
+  std::default_random_engine re;
+  re.seed(time(NULL)); 
+  std::uniform_int_distribution<int> uni(1, 99);
 
+  for (int i=0; i<num; i++) {
+    int randomNum = uni(re);
+    trans = trans + items[randomNum] + " ";
+  }
   return trans
 }
 
@@ -23,14 +52,15 @@ string GenTransaction(int num) {
 int main() {
   std::vector<std::string> items;
 
-//Random int generator for transaction amount
-  std::default_random_engine re;
-  re.seed(time(NULL)); 
-  std::uniform_int_distribution<int> uni(5, 15);
-
-  int randomNum = uni(re);
-
+  //Create vector of "items"
   populateVector(vector items);
 
+  //Create databases
+  GenDatabase1K();
+  GenDatabase10K();
+  GenDatabase50K();
+  GenDatabase100K();
+
+  return 0;
 }
 
