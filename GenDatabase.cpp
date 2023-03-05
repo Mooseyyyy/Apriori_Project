a132c6581f53da452d3b2a08001460a96879cdbf
Mooseyyyy <hoekb24@gmail.com>
2023-03-05 10:58:40 -0700

    Fix compile issues

--- GenDatabase.cpp
+++ GenDatabase.cpp
@@ -1,15 +1,45 @@
-#include "GenDatabase.h"
 #include <random>
 #include <vector>
 #include <string>
+#include <ctime>
 
-void GenDatabase1K () {
+int genRandomInt () {
+  //Random int generator for transaction amount
+  std::default_random_engine re;
+  re.seed(time(NULL));
+  std::uniform_int_distribution<int> uni(5, 15);
+
+  int randomNum = uni(re);
+
+  return randomNum;
+}
+
+void populateVector(std::vector<std::string>& items) {
+    for(int i = 0; i<100; i++) {
+        items[i]="i" + std::to_string(i);
+    }
+}
+
+std::string GenTransaction(int num, std::vector<std::string>& items) {
+  std::string trans;
+  std::default_random_engine re;
+  re.seed(time(NULL));
+  std::uniform_int_distribution<int> uni(1, 99);
+
+  for (int i=0; i<num; i++) {
+    int randomNum = uni(re);
+    trans = trans + items[randomNum] + " ";
+  }
+  return trans;
+}
+
+void GenDatabase1K (std::vector<std::string>& items) {
   std::string output;
   //Create file
   //Open File
   for (int i = 0; i<1000; i++) {
     int transaction = genRandomInt();
-    output = GenTransaction(transaction); 
+    output = GenTransaction(transaction, items);
     //Write output to file
   }
   //Close File
@@ -18,49 +48,18 @@ void GenDatabase10K () {}
 void GenDatabase50K () {}
 void GenDatabase100K () {}
 
-int genRandomInt () {
-  //Random int generator for transaction amount
-  std::default_random_engine re;
-  re.seed(time(NULL)); 
-  std::uniform_int_distribution<int> uni(5, 15);
-
-  int randomNum = uni(re);
-  
-  return randomNum;
-}
-
-void populateVector(vector<std::string>& items) {
-    for(int i = 0; i<100; i++) {
-        items[i]="i" + std::to_string(i);
-    }
-}
-
-string GenTransaction(int num) {
-  std::string trans;
-  std::default_random_engine re;
-  re.seed(time(NULL)); 
-  std::uniform_int_distribution<int> uni(1, 99);
-
-  for (int i=0; i<num; i++) {
-    int randomNum = uni(re);
-    trans = trans + items[randomNum] + " ";
-  }
-  return trans
-}
-
 
 int main() {
   std::vector<std::string> items;
 
   //Create vector of "items"
-  populateVector(vector items);
+  populateVector(items);
 
   //Create databases
-  GenDatabase1K();
+  GenDatabase1K(items);
   GenDatabase10K();
   GenDatabase50K();
   GenDatabase100K();
 
   return 0;
 }
-
---
+++ Makefile
@@ -0,0 +1,17 @@
+
+CCC= g++
+CCCFLAGS = -Wall -std=c++11
+
+all : database
+database : GenDatabase.o
+	$(CCC) $(CCCFLAGS) $^ -o $@
+
+# default rule for compiling .cc to .o
+%.o: %.cc
+	$(CCC) -c $(CCCFLAGS) $<
+
+clean:
+	rm -f *.o *~ *% *# .#*
+
+clean-all: clean
+	rm -f database
