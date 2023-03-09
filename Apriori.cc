5e4c937c607f50e0ea9e604e8b0d551bf72dfec3
Mooseyyyy <hoekb24@gmail.com>
2023-03-09 12:34:47 -0700

    Testing txt file input

---
+++ Apriori.cc
@@ -0,0 +1,32 @@
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+class Database {
+public:
+    Database();
+    openDatbase1K() {
+        database.open("Database1K.txt")
+    }
+
+    std::vector<std::string> transactions;
+    std::ofstream database;
+};
+
+void test(Datbase db, float ms) {
+    std::string fileCompare, loopCompare;
+    while (db.database >> fileCompare) {
+        std::cout << fileCompare << " ";
+    }
+}
+
+int main() {
+    Database db;
+    float i = 1.0;
+    db.openDatbase1K();
+
+    test(db, i);
+
+    return 0;
+}
