fac15e4b63cc858554f1e6e14d0294dd0c46149d
brenthawkings <brent@brenthawk.com>
2023-03-14 18:56:26 -0600

    Redesign of openDatabase method of Database object

--- Apriori.cc
+++ Apriori.cc
@@ -6,12 +6,22 @@
 class Database {
 public:
     Database();
-    void openDatabase1K() {
-        database.open("Database1K.txt");
+    void openDatabase(std::string dbf) {
+        
+        
+std::string line;
+        std::ifstream database;
+        database.open(dbf);
+        if (database.is_open()) {
+            while (getline(database, line)) {
+                std::cout << line << '\n';
+            }
+        database.close();
+        } else std::cout << "Unable to open file";
     }
 
     std::vector<std::string> transactions;
-    std::ofstream database;
+    
 };
 
 class itemsets {
@@ -21,15 +31,15 @@ public:
 
 itemsets apriori(Database &db, float ms) {
     std::string fileCompare, loopCompare;
-    while (db.database) {
-        std::cout << fileCompare << " ";
+    for (int i = 0; i < db.transactions.size(); i++) {
+        
     }
 }
 
 int main() {
     Database db;
     float i = 1.0;
-    db.openDatabase1K();
+    db.openDatabase("Database1K.txt");
 
     apriori(db, i);
 
