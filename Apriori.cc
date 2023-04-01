37550b79625c9015ba46f0a0a223b967e6ce5b1a
brenthawkings <brent@brenthawk.com>
2023-03-31 20:11:20 -0600

    Working but slow AF

--- Apriori.cc
+++ Apriori.cc
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <algorithm>
 #include <regex>
+#include <iomanip>
 
 using namespace std;
 
@@ -383,8 +384,8 @@ int main()
   time(&start);
   ios_base::sync_with_stdio(false);
 
-  // vector<string> test = {"i1 i2 i3 i4 i5 i6", "i2 i3 i4 i5 i6 i7", "i1 i4 i5 i8", "i1 i4 i6 i9 i10", "i2 i4 i5 i10 i11"};
-  // apriori(test, 0.6);
+  vector<string> test = {"i1 i2 i3 i4 i5 i6", "i2 i3 i4 i5 i6 i7", "i1 i4 i5 i8", "i1 i4 i6 i9 i10", "i2 i4 i5 i10 i11"};
+  apriori(test, 0.6);
 
   // vector<string> db1 = openDatabase("Database1K.txt");
   // apriori(db1, 0.01);
@@ -395,8 +396,8 @@ int main()
   // vector<string> db50 = openDatabase("Database50K.txt");
   // apriori(db50, 0.01);
 
-  vector<string> db100 = openDatabase("Database100K.txt");
-  apriori(db100, 0.01);
+  // vector<string> db100 = openDatabase("Database100K.txt");
+  // apriori(db100, 0.01);
 
   /*
   int main(int argc, char *argv[]) {
